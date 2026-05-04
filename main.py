f0f8da2ad550d1a2d45a7131e9521cf51612ce9f
Rishabh Prajapati <prajapatirishabh04@gmail.com>
2026-05-04 14:17:22 +0530

    Add config endpoint to return environment variable message

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from fastapi import FastAPI
-
+import os
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Hello World from rishabh's backend app version star kid!"}
+
+@app.get("/config")
+def read_config():
+    my_name = os.getenv("TEST_ENV_NAME", "Default Name")
+    return {"message": f"Hello {my_name} from the backend app!"}
+
